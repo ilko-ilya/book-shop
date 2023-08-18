729e00214293dd4495c424c3926abc5e97ebc65a
Ilya Samilyak <samilyakilya@gmail.com>
2023-08-18 20:44:38 +0300

    HW-spring-boot-book_shop

---
+++ src/main/java/mate/academy/bookshop/repository/BookRepositoryImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.bookshop.repository;
+
+import java.util.List;
+import mate.academy.bookshop.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add book: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> bookQuery = session.createQuery("FROM Book ", Book.class);
+            return bookQuery.getResultList();
+        }
+    }
+}
